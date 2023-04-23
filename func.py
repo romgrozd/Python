79801de7d8b53828d3e4ab4e3a03f52dbd6a7ae1
roman <kalibri_grozd@mail.ru>
2023-04-23 17:34:25 +0300

    Did HW8

--- func.py
+++ func.py
@@ -1,12 +1,17 @@
+from sqlite3 import InterfaceError
+
+from interface import get_contact
+
+
 def add_contact(contact):
     #contact = [input("Введите имя: ")+" ", input("Введите фамилию: ")+" ", input("Введите отечество: ")+ " ", input("Введите номер телефона: ")]
-    data = open('file.txt', 'a')
+    data = open('file.txt', 'a', encoding='utf-8')
     data.writelines(contact)
     data.write('\n')
     data.close()
 
 def search_contact():
-    contact_list = open('file.txt', 'r')
+    contact_list = open('file.txt', 'r', encoding='utf-8')
     search = input("Введите фамилию для поиска: ")
     for i in contact_list:
          if search in i:
@@ -14,12 +19,41 @@ def search_contact():
     contact_list.close()
 
 def print_all():
-    contact_list = open('file.txt', 'r')
+    contact_list = open('file.txt', 'r', encoding='utf-8')
     num = 1
     for i in contact_list:
             print(f'{num}. {i}')
             num +=1
-    contact_list.close()            
+    contact_list.close()    
+
+def del_contact(search):
+    contact_list = open('file.txt', 'r', encoding='utf-8')
+    cash_list = []
+    for i in contact_list:
+        if search in i:
+                continue
+        cash_list.append(i)
+    contact_list.close()
+
+    contact_list = open('file.txt', 'w', encoding='utf-8')
+    for i in cash_list:
+        contact_list.writelines(i)
+    contact_list.close()
+
+def change_contact(search):
+    contact_list = open('file.txt', 'r', encoding='utf-8')
+    cash_list = []
+    for i in contact_list:
+        if search in i:
+            cash_list.append(get_contact())
+            continue
+        cash_list.append(i)
+    contact_list.close()
+
+    contact_list = open('file.txt', 'w', encoding='utf-8')
+    for i in cash_list:
+        contact_list.writelines(i)
+    contact_list.close()
 
       
         
